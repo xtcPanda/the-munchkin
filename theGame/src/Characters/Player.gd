b638070ceae191965d13124c2a0aaa50357556ed
Moaaz Akram Alhajj <41624372+xtcPanda@users.noreply.github.com>
2020-05-03 21:47:24 +0200

    fixed the jumping issue
    it was just the place of the speed and direction in the calculate func

--- theGame/src/Characters/Player.gd
+++ theGame/src/Characters/Player.gd
@@ -8,17 +8,17 @@ func _physics_process(delta: float) -> void:
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
 	)
 
 func calculate_move_velocity(
 	linear_velocity: Vector2,
-	speed: Vector2,
-	direction: Vector2
+	direction: Vector2,
+	speed: Vector2
 ):
 	var new_velocity: = linear_velocity
 	new_velocity.x = speed.x * direction.x
 	new_velocity.y += gravity * get_physics_process_delta_time()
-	if direction.y == 0.0:
+	if direction.y == -1.0:
 		new_velocity.y = speed.y * direction.y
 	return new_velocity
