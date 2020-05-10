1a42ebcd9b008a55154151095aa0f7be7188e55a
Moaaz Akram Alhajj <41624372+xtcPanda@users.noreply.github.com>
2020-05-10 18:07:28 +0200

    finishing the 1st tutorial

--- theGame/src/Characters/Character.gd
+++ theGame/src/Characters/Character.gd
@@ -5,12 +5,12 @@ class_name Character
 export var speed: = Vector2(400.0, 500.0)
 export var gravity: = 3500.0
 
-var velocity: = Vector2.ZERO
+var _velocity: = Vector2.ZERO
 const FLOOR_NORMAL = Vector2.UP
 
 #func _physics_process(delta: float) -> void:
-#		velocity.y = max(velocity.y, speed.y)
+#		_velocity.y = max(_velocity.y, speed.y)
 ##		you can do the same with if
-##		if(velocity.y > speed.y):
-##			velocity.y = speed.y
+##		if(_velocity.y > speed.y):
+##			_velocity.y = speed.y
 
